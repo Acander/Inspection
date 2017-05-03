fd310a1b8f5d680be10ec30196b55f5e9c8144b4
Adrian <Adrian@AcanderToshiba5>
2017-05-03 12:39:36 +0200

    update 25

--- Inspection1/src/se/kth/ict/Inspection1/view/View.java
+++ Inspection1/src/se/kth/ict/Inspection1/view/View.java
@@ -26,27 +26,50 @@ public class View {
 	
 	public void sampleExecution()
 	{
+		System.out.println("*Display que number and opening garage door.");
 		controller.specifyThatAnewInspectionIsAboutToTakePlace(2, 5);
+		System.out.println();
+		System.out.println("*Closing garage door.");
 		controller.closeGarageDoor();
+		System.out.println();
+		
+		System.out.println("*Registration number is entered and cost is calculated.");
 		double cost = controller.enterRegNumberToProduceCostAndInspectionList("MGD545");
+		System.out.println("Inspectior sais: Cost for the inspection is "+cost+ "kr.");
+		System.out.println();
+		
+		System.out.println("*Customer hands over payment.");
+		System.out.println("*Amount of Cash is registrered.");
+		System.out.println("*Receipt is printed.");
+		System.out.println();
 		System.out.print("Receipt 1: (Cash)\n");
 		double change = controller.registerAmountOfCashPaid(3000);
+		System.out.println();
+		System.out.println("*In the even of a creditCard payment: The customer enter credit card information.");
+		System.out.println("*Receipt is printed");
 		System.out.println();
 		System.out.print("Receipt 2: (Creditcard)\n");
 		controller.enterCreditCardInforMation(new CreditCard(3456, "62783945473", "this", new Date(2, 17), 871));
 		
 		System.out.println();
+		System.out.println("*The inspectors checklist is inspected and a result list is created in the system.");
 		InspectionCheckList checkList = controller.specifyWhatToInspect();
 		System.out.println("Inspection Checklist:");
 		System.out.println(checkList.toString());
 		
+		System.out.println("*Individual results are written in.");
+		System.out.println("*When list is full the results are shown.");
+		System.out.println();
 		controller.enterResult(true);
 		controller.enterResult(false);
 		controller.enterResult(false);
 		System.out.println("Results:");
 		controller.printResults(controller.getResults());
 		
+		System.out.println();
+		System.out.println("*Opening Garage door");
 		controller.openGarageDoor();
+		System.out.println("*Closing Garage door");
 		controller.closeGarageDoor();
 	}
 
